5a5e04d7528040afc8d2acde6ef11120a880de88
Yankov Vlad <vladankov996@gmail.com>
2021-07-30 23:22:17 +0300

    fix: fix solution tasks

--- src/H9.1-ModArr.js
+++ src/H9.1-ModArr.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import React from 'react';
-import './names.json'
+import names from './names.json'
 
 export default class ModArr extends React.Component {
   state = {
@@ -9,24 +9,22 @@ export default class ModArr extends React.Component {
   }
 
   onClickGetName = () => {
-    const names = require('./names.json')
     const index = Math.floor(Math.random() * 13)
-    const oneMoreName = names.filter((name, ind) => ind === index)
-    const newPeoples =[...this.state.peoples, oneMoreName]
-    this.setState({peoples: newPeoples})
-    
+    const oneMoreName = names[index]
+    const newPeoples = [...this.state.peoples, oneMoreName]
+    this.setState({ peoples: newPeoples })
+
   }
 
-  render () {
+  render() {
     return (
       <div className={'wrap'}>
         <button onClick={this.onClickGetName}>Add new name</button>
-          <ul className={'list'}>
-            {this.state.peoples.map((name) => <li>{name}</li>)}
-          </ul>
-          
+        <ul className={'list'}>
+          {this.state.peoples.map((name) => <li key={name}>{name}</li>)}
+        </ul>
       </div>
-      
+
     )
   }
 };
--- src/H9.3-useEffect.js
+++ src/H9.3-useEffect.js
@@ -2,52 +2,40 @@ import { useState, useEffect } from "react"
 
 export const TimePicker = (props) => {
 
-    const [hour, setHour] = useState({decadeHour: 0, hour: 0})
-    const [minute, setMinute] = useState({decadeMinute: 0, minute: 0})
+    const [hour, setHour] = useState(0)
+    const [minute, setMinute] = useState(0)
 
     useEffect(() => {
-        if (minute.minute >= 10) {setMinute({decadeMinute: minute.decadeMinute + 1, minute: 0})}
-        if (minute.decadeMinute === 6 && minute.minute === 0) {
-            setHour({decadeHour: hour.decadeHour, hour: hour.hour + 1})
-            setMinute({decadeMinute: 0, minute: 0})
+        if (hour >= 24) { setHour(0) }
+        if (minute >= 60) {
+            setHour(hour + 1)
+            setMinute(0)
         }
-        if (hour.hour >= 10) {setHour({decadeHour: hour.decadeHour + 1, hour: 0})}
-        if (hour.hour < 0) {setHour({decadeHour: hour.decadeHour - 1, hour: 9})}
-        if (hour.decadeHour === 2 && hour.hour > 3) {setHour({decadeHour: 0, hour: 0})}
-
-        if (hour.decadeHour === 0 && hour.hour < 0) {setHour({decadeHour: 2, hour: 3})}
-        if (minute.minute < 0) {setMinute({decadeMinute: minute.decadeMinute - 1, minute: 9})}
-        if (minute.decadeMinute === 0 && minute.minute < 0) {
-            setHour({decadeHour: hour.decadeHour, hour: hour.hour - 1})
-            setMinute({decadeMinute: 5, minute: 9})
+        if (hour >= 23 && minute >= 60) {
+            setHour(0)
+            setMinute(0)
         }
-        if (hour.decadeHour === 0 && hour.hour === 0 && minute.decadeMinute === 0 && minute.minute < 0) {
-            setHour({decadeHour: 2, hour: 3})
-            setMinute({decadeMinute: 5, minute: 9})
+        if (hour < 0) { setHour(23) }
+        if (minute < 0) {
+            setMinute(59)
+            setHour(hour - 1)
         }
-
     }, [hour, minute])
 
     const onClickHour = (value) => {
-        let newHour = {}
-        newHour = Object.assign(newHour, hour)
-        newHour.hour = newHour.hour + value
-        setHour(newHour)
+        setHour(hour + value)
     }
     const onClickMinute = (value) => {
-        let newMinute = {}
-        newMinute = Object.assign(newMinute, minute)
-        newMinute.minute = newMinute.minute + value
-        setMinute(newMinute)
+        setMinute(minute + value)
     }
 
 
     return (
         <div className={'wrap'}>
             <div className={'clock'}>
-                <div className={'hour'}>{hour.decadeHour}{hour.hour}</div>
+                <div className={'hour'}>{hour < 10 ? '0' + hour : hour}</div>
                 <div className={'clock-dublepoint'}>:</div>
-                <div className={'minute'}>{minute.decadeMinute}{minute.minute}</div>
+                <div className={'minute'}>{minute < 10 ? '0' + minute : minute}</div>
                 <div className={'clock-hour-button'}>
                     <button onClick={() => onClickHour(props.value)} className={'btn hour-plus'}>+</button>
                     <button onClick={() => onClickHour(-props.value)} className={'btn hour-minus'}>-</button>
@@ -56,7 +44,7 @@ export const TimePicker = (props) => {
                     <button onClick={() => onClickMinute(props.value)} className={'btn minute-plus'}>+</button>
                     <button onClick={() => onClickMinute(-props.value)} className={'btn minute-plus'}>-</button>
                 </div>
-               
+
             </div>
         </div>
     )
